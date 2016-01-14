0fdfb8e8c098309be3234bdda520c6636bc20e92
zhufengpeixun <zhufengpeixun@126.com>
2016-01-14 13:08:49 +0800

    promise ajax

--- 4.ajax/2.ajaxpost/server.js
+++ 4.ajax/2.ajaxpost/server.js
@@ -6,10 +6,23 @@
 var students = [];
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 http.createServer(function(req,res){
-    fs.readFile('./index.html',function(err,data){
-        res.write(data);
-        res.end();
-    })
+    var urlObj = url.parse(req.url,true);// true query是json false  query字符串
+    console.log(urlObj);
+    var pathname = urlObj.pathname;
+    console.log(pathname);
+    if(pathname == '/'){
+        fs.readFile('./index.html',function(err,data){
+            res.write(data);
+            res.end();
+        })
+    }else if(pathname == '/ajax'){
+        students.push(urlObj.query);
+        res.end(JSON.stringify(students));
+    }else{
+        res.end('404');
+    }
+
 }).listen(8080);
 
